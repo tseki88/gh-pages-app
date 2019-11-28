7f80b46bcd0999bb40eaa227cdd8d32bc7ca06cc
tseki88 <teruhisaseki@gmail.com>
2019-11-28 10:51:09 -0500

    Updates

---
+++ precache-manifest.a0fef44368270fcc8726e16431b180fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51dc98e8b320cb00eb0f7dbe1945c814",
+    "url": "/gh-pages-app/index.html"
+  },
+  {
+    "revision": "60cb6054a92cdd16689a",
+    "url": "/gh-pages-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "566977cd107981fa4003",
+    "url": "/gh-pages-app/static/js/2.f0e4f202.chunk.js"
+  },
+  {
+    "revision": "60cb6054a92cdd16689a",
+    "url": "/gh-pages-app/static/js/main.442042a7.chunk.js"
+  },
+  {
+    "revision": "e165b08d794ffde0ad3d",
+    "url": "/gh-pages-app/static/js/runtime-main.2c74c748.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gh-pages-app/static/media/logo.25bf045c.svg"
+  }
+]);
